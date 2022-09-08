2991e7e7db6fdb22856fd82381f312813edc4479
davide-caffaratti <82267325+davide-caffaratti@users.noreply.github.com>
2022-09-09 00:07:48 +0200

    Update config.php
    Modified config file with new definitions

--- backend-php/config.php
+++ backend-php/config.php
@@ -1,37 +1,31 @@
-<?php
+<<?php
 
 /* note that all _URL and _DIR configurations below must end with a forward slash (/) */
 
-$config = [
+$config = array(
 
-	/* Url for image serving in final download */
-	SERVE_URL => "https://YOUR_EMAIL_IMAGE_SERVER_URL/",
+    /* Url for image serving in final download */
+    'SERVE_URL' => 'http://localhost/newsletter/media/',
 
-	/* Base Url for accessing Mosaico */
-	BASE_URL => "http://YOUR_MOSAICO_URL/",
-	
-	/* local file system base path to where image directories are located */
-	BASE_DIR => "/var/www/mosaico/",
-	
-	/* url to the uploads folder (relative to BASE_URL) */
-	UPLOADS_URL => "uploads/",
-	
-	/* local file system path to the uploads folder (relative to BASE_DIR) */
-	UPLOADS_DIR => "uploads/",
-	
-	/* url to the static images folder (relative to SERVE_URL) */
-	STATIC_URL => "media/newsletter/static/",
+    /* Base path for image serving in final download */
+    'SERVE_PATH' => $_SERVER['DOCUMENT_ROOT'] . '/newsletter/media/',
 
-	/* local file system path to the static images folder (relative to BASE_DIR) */
-	STATIC_DIR => "uploads/static/",
-	
-	/* url to the thumbnail images folder (relative to BASE_URL */
-	THUMBNAILS_URL => "uploads/thumbnail/",
-	
-	/* local file system path to the thumbnail images folder (relative to BASE_DIR) */
-	THUMBNAILS_DIR => "uploads/thumbnail/",
-	
-	/* width and height of generated thumbnails */
-	THUMBNAIL_WIDTH => 90,
-	THUMBNAIL_HEIGHT => 90
-];
+    /* Local file system path to the static images folder (relative to BASE_PATH) */
+    'STATIC_DIR' => 'static/',
+
+    /* Base Url for accessing Mosaico */
+    'BASE_URL' => 'http://localhost/newsletter/',
+
+    /* Local file system base path to where image directories are located */
+    'BASE_PATH' => $_SERVER['DOCUMENT_ROOT'] . '/newsletter/',
+
+    /* Dir name for the uploads folder (relative to BASE_URL and BASE_PATH) */
+    'UPLOADS_DIR' => 'uploads/',
+
+    /* Dir name for the thumbnail images folder (relative to BASE_URL and BASE_PATH) */
+    'THUMBNAILS_DIR' => 'uploads/thumb/',
+
+    /* width and height of generated thumbnails */
+    'THUMBNAIL_WIDTH' => 90,
+    'THUMBNAIL_HEIGHT' => 90
+);
