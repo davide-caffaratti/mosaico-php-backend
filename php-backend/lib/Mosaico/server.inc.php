17d4ebc83b297d99cd233b71582dabdd155d1640
davide-caffaratti <82267325+davide-caffaratti@users.noreply.github.com>
2022-09-19 17:13:16 +0200

    Create server.inc.php
    The main class for Mosaico server

---
+++ php-backend/lib/Mosaico/server.inc.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @desc Main class for manage Mosaico php backend
+ *
+ * @class Mosaico_Server
+ * @package Mosaico
+ * @use Mosaico_ProcessUploadRequest, Mosaico_ProcessImgRequest, Mosaico_ProcessDlRequest, Mosaico_ProcessTplRequest, Mosaico_ResizeImage, Mosaico_CheckEmail, Mosaico_ServerError
+ * @author Davide Caffaratti <davcaffa@gmail.com>
+ * @version 1.0
+ */
+class Mosaico_Server
+{        
+    /**
+     * Public constructor
+     * @access public static
+     */
+    public static function get()
+    {
+        global $config;
+        
+        $url = parse_url($_SERVER['REQUEST_URI']);
+        $dir = substr($url['path'], strlen(dirname($url['path'])));
+        
+        switch($dir) {
+            case '/uploads/':
+                Mosaico_ProcessUploadRequest::get();
+            break;
+            case '/img/':
+                Mosaico_ProcessImgRequest::get();
+            break;
+            case '/dl/':
+                Mosaico_ProcessDlRequest::get();
+            break;
+            case '/tpl/':
+                Mosaico_ProcessTplRequest::get();
+            break;
+            default:
+                Mosaico_ServerError::set(404);
+        }        
+    }
+}
