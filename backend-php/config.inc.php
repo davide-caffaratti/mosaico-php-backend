482be7faf69eb3398827756f6b4486db989132f1
davide-caffaratti <82267325+davide-caffaratti@users.noreply.github.com>
2022-09-19 16:51:10 +0200

    Update config.inc.php
    Add new config var and a method for protect all the script from unallowed users

--- backend-php/config.inc.php
+++ backend-php/config.inc.php
@@ -1,31 +1,162 @@
-<?php
+<?php      
+/**
+* config.inc.php (Configuration for Mosaico - Responsive Email Template Editor)
+*/
 
-/* note that all _URL and _DIR configurations below must end with a forward slash (/) */
+/*
+|--------------------------------------------------------------------------
+| USE THIS FUNCTION FOR PROTECT YOUR IMAGE UPLOAD SCRIPT
+|--------------------------------------------------------------------------
+*/
+function WY_Protect() 
+{      
+   if (!isset($_SESSION['your_user_id_logged_in'])) {
+       trigger_error('Invalid user try to access the images manager system !!', E_USER_WARNING);
+       exit;
+   }
+}
+/*
+|--------------------------------------------------------------------------
+| ACTIVATE THE FUNCTION ABOVE
+|--------------------------------------------------------------------------
+*/
+//WY_Protect();
 
 $config = array(
-
-    /* Url for image serving in final download */
-    'SERVE_URL' => 'http://localhost/mosaico/media/',
-
-    /* Base path for image serving in final download */
-    'SERVE_PATH' => $_SERVER['DOCUMENT_ROOT'] . '/mosaico/media/',
-
-    /* Local file system path to the static images folder (relative to BASE_PATH) */
+    /*
+    |--------------------------------------------------------------------------
+    | Mosaico lib version
+    |--------------------------------------------------------------------------
+    */
+    'MOSAICO_LIB_VERSION'=> '0.18.6',    
+    /*
+    |--------------------------------------------------------------------------
+    | Flag for activate the download html email function
+    |--------------------------------------------------------------------------
+    */
+    'USE_DOWNLOAD_HTML' => true,   
+    /*
+    |--------------------------------------------------------------------------
+    | Flag for activate the save templates function
+    |--------------------------------------------------------------------------
+    */
+    'USE_SAVE_TEMPLATES' => true, 
+    /*
+    |--------------------------------------------------------------------------
+    | Base Url for Mosaico main folder with final /
+    |--------------------------------------------------------------------------
+    */
+    'BASE_URL' => 'http://localhost/newsletter/',
+    /*
+    |--------------------------------------------------------------------------
+    | Base path for Mosaico main folder with final /
+    |--------------------------------------------------------------------------
+    */
+    'BASE_PATH' => $_SERVER['DOCUMENT_ROOT'] . '/newsletter/',
+    /*
+    |--------------------------------------------------------------------------
+    | Url where the Mosaico server backend folder is located with final /
+    |--------------------------------------------------------------------------
+    */
+    'PHP_SERVER_URL' => 'http://localhost/newsletter/backend-php/',
+    /*
+    |--------------------------------------------------------------------------
+    | Url for the folder of the newsletters public images with final /
+    |--------------------------------------------------------------------------
+    */
+    'SERVE_IMG_URL' => 'http://localhost/newsletter/media/',
+    /*
+    |--------------------------------------------------------------------------
+    | Path for the folder of the newsletters public images with final /
+    |--------------------------------------------------------------------------
+    */
+    'SERVE_IMG_PATH' => $_SERVER['DOCUMENT_ROOT'] . '/newsletter/media/',
+    /*
+    |---------------------------------------------------------------------------------------
+    | Path for mosaico library directory (relative to BASE_URL and BASE_PATH) with final /
+    |---------------------------------------------------------------------------------------
+    */
+    'LIBRARY_DIR'=> 'dist/rs/',
+    /*
+    |-------------------------------------------------------------------------------
+    | Resources images directory (relative to BASE_URL and BASE_PATH) with final /
+    |-------------------------------------------------------------------------------
+    */
+    'IMAGES_DIR'=>'dist/rs/img/',
+    /*
+    |----------------------------------------------------------------------------------
+    | Resources languages directory (relative to BASE_URL and BASE_PATH) with final /
+    |----------------------------------------------------------------------------------
+    */
+    'LANG_DIR'=>'dist/rs/lang/',
+    /*
+    |--------------------------------------------------------------------------
+    | Used language for editor.php and index.php
+    |--------------------------------------------------------------------------
+    */
+    'USED_LANG'=>'en',
+    /*
+    |-------------------------------------------------------------------------------------
+    | Used language file full filename for json translation in the "LANG_DIR" directory
+    | (if set empty, mosaico intreface, use the default language)
+    |-------------------------------------------------------------------------------------
+    */
+    'LANG_FILE'=>'',
+    /*
+    |-------------------------------------------------------------------------------------------------------
+    | Local file system path to the static images folder (relative to BASE_URL and BASE_PATH) with final /
+    |-------------------------------------------------------------------------------------------------------
+    */
     'STATIC_DIR' => 'static/',
-
-    /* Base Url for accessing Mosaico */
-    'BASE_URL' => 'http://localhost/mosaico/',
-
-    /* Local file system base path to where image directories are located */
-    'BASE_PATH' => $_SERVER['DOCUMENT_ROOT'] . '/mosaico/',
-
-    /* Dir name for the uploads folder (relative to BASE_URL and BASE_PATH) */
+    /*
+    |-------------------------------------------------------------------------------------------------------
+    | Local file system path to the images upload folder (relative to BASE_URL and BASE_PATH) with final /
+    |-------------------------------------------------------------------------------------------------------
+    */
     'UPLOADS_DIR' => 'uploads/',
-
-    /* Dir name for the thumbnail images folder (relative to BASE_URL and BASE_PATH) */
+    /*
+    |-----------------------------------------------------------------------------------------------------------------
+    | Local file system path to the thumbnail images upload folder (relative to BASE_URL and BASE_PATH) with final /
+    |-----------------------------------------------------------------------------------------------------------------
+    */
     'THUMBNAILS_DIR' => 'uploads/thumb/',
-
-    /* width and height of generated thumbnails */
+    /*
+    |--------------------------------------------------------------------------
+    | Width and Height of generated thumbnails
+    |--------------------------------------------------------------------------
+    */
     'THUMBNAIL_WIDTH' => 90,
-    'THUMBNAIL_HEIGHT' => 90
+    'THUMBNAIL_HEIGHT' => 90,
+    /*
+    |--------------------------------------------------------------------------
+    | Default template dir (relative to BASE_URL and BASE_PATH) without final /
+    |--------------------------------------------------------------------------
+    */
+    'DEF_TEMPLATE_BASEDIR'=> 'versafix-1.it',  
+    /*
+    |-------------------------------------------------------------------------------------------------
+    | Field name for user_id that we get from $_SESSION in the save templates function
+    | If we set to 0, system set the user_id to 0 in the Mosaico template database
+    | Setting: 
+    |         Set in your script the var $_SESSION['your_user_id_logged_in'] with the actual user_id
+    |         'SESSION_USER_ID' => (isset($_SESSION['your_user_id_logged_in']) ? $_SESSION['your_user_id_logged_in'] : 0), 
+    |         OR The script set in the database the user_id => 0 (Only one user for all templates)
+    |         'SESSION_USER_ID' => 0,    
+    | Use in the script:     $user_id = $config['SESSION_USER_ID'];
+    |-------------------------------------------------------------------------------------------------
+    */
+    'SESSION_USER_ID' => (isset($_SESSION['your_user_id_logged_in']) ? $_SESSION['your_user_id_logged_in'] : 0),  
+    /*
+    |--------------------------------------------------------------------------
+    | Database configuration
+    | Used only if USE_SAVE_TEMPLATES is set to true
+    |--------------------------------------------------------------------------
+    */
+    'DB_USE_UTF_8'=>true,
+    'DB' => 'malvanomarchesini',
+    'DB_HOST' => 'localhost',
+    'DB_USER' => 'root',
+    'DB_PASS' => '',
+    'DB_TABLE' => 'cat_mosaico_tpl',
+    'DB_DATE_FORMAT' => '%d/%m/%Y' // Mysql date format use by the query for extract templates DATE_FORMAT(date,'%d/%m/%Y') AS 
 );
